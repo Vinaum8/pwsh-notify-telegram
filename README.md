675c6a2e3716615be2d0f001ecff346874893926
vinicius.fernandes <vinicius.fernandes@useall.com.br>
2022-03-27 20:22:11 -0300

    documentando o script

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
-# shelzin-da-massa
+# pwsh-notify-telegram
 runs the script and notifies you if an error occurs
 
-* define the envinroment variable
-    * telegram_token
-    * telegram_chat_id
+1: define the envinroment variable
+* telegram_token
+* telegram_chat_id
--- try-catch.ps1
+++ try-catch.ps1
@@ -1,3 +1,21 @@
+<# Script para enviar mensagens ao telegram via powershell.
+    Autor: Vinicius dos Santos Fernandes
+    Link do repositório: https://github.com/Vinaum8/pwsh-notify-telegram
+# Contatos:
+        # Email: vinaumpt@gmail.com
+        # Linkedin: https://www.linkedin.com/in/vinisf/
+# Version: 1.0
+#
+# -------------------------------------------------------------------------- #
+#
+# Este script basicamente coleta as variáveis de ambiente do telegram setadas no seu servidor ou container e envia a mensagem de retorno do try-catch para o chat ID.
+#
+# Exemplos de Execução:
+#       $ ./try-catch.ps1
+        Você também pode executar este script via docker com a imagem https://hub.docker.com/_/microsoft-powershell
+
+#>
+
 $global:servername = $(Get-CimInstance -ClassName Win32_ComputerSystem).Name
 
 try {
@@ -9,6 +27,6 @@ try {
     $MyToken = $([System.Environment]::GetEnvironmentVariables().telegram_token)
     $chatID = $([System.Environment]::GetEnvironmentVariables().telegram_chat_id)
 
-    $Message = "Ocorreu erro realizando um backup!`n`nServidor: $servername`n`n" + "Erro: $deuruim"
+    $Message = "Erro! => `n`n Servidor: $servername`n`n" + "Erro: $deuruim"
     Invoke-RestMethod -Uri "https://api.telegram.org/bot$($MyToken)/sendMessage?chat_id=$($chatID)&text=$($Message)"
 }
